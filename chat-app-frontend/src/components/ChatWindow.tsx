1c79fe9f8558c4b14bab314d7b64b1ee1cf3d8f3
shipon-hossen-raju <msshipon234@gmail.com>
2025-06-21 21:04:02 +0600

    chating update video & audio

--- chat-app-frontend/src/components/ChatWindow.tsx
+++ chat-app-frontend/src/components/ChatWindow.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { useUser } from "../context/UserContext";
 import { connectSocket, getSocket } from "../lib/socket";
+import CallingFeature from "./CallingFeature";
 
 type TMessage = {
   createdAt?: string;
@@ -23,8 +24,7 @@ const ChatWindow = ({ receiverId }: { receiverId: string }) => {
   const [messages, setMessages] = useState<TMessage[] | TAIMessage[]>([]);
   const [input, setInput] = useState("");
   const { user } = useUser();
-
-  console.log("ChatWindow messages: ", messages);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     const socket = connectSocket(user?.id || "");
@@ -34,7 +34,7 @@ const ChatWindow = ({ receiverId }: { receiverId: string }) => {
     });
 
     socket.on("receiveMessage", (msg: TMessage) => {
-      console.log("Received message: ", msg);
+      setLoading(false);
       setMessages((prev) => [...prev, msg]);
     });
 
@@ -48,8 +48,10 @@ const ChatWindow = ({ receiverId }: { receiverId: string }) => {
     const socket = getSocket();
     if (!socket) return;
     socket.emit("joinRoom", { userId: user?.id, senderId: receiverId });
+
+    setLoading(true);
     socket.on("joinRoom", (msgs) => {
-      console.log("Joined room messages: ", msgs);
+      setLoading(false);
       setMessages(msgs);
     });
   }, [receiverId]);
@@ -59,9 +61,11 @@ const ChatWindow = ({ receiverId }: { receiverId: string }) => {
     if (receiverId === "ai-chat") {
       const socket = getSocket();
       if (!socket) return;
+      setLoading(true);
       socket.emit("aiJoinRoom", { userId: user?.id, senderId: receiverId });
       socket.on("aiJoinRoom", (msgs) => {
         setMessages(msgs);
+        setLoading(false);
       });
     }
   }, [receiverId]);
@@ -69,17 +73,32 @@ const ChatWindow = ({ receiverId }: { receiverId: string }) => {
   const sendMessage = () => {
     const socket = getSocket();
     if (!socket) return;
+    setLoading(true);
     socket.emit("sendMessage", { input, to: receiverId });
     setInput("");
   };
 
   return (
-    <div className="p-4 max-w-md mx-auto bg-white shadow-md rounded" style={{ width: "50%" }}>
+    <div
+      className="p-4 max-w-md mx-auto bg-white shadow-md rounded"
+      style={{ width: "50%" }}
+    >
       <div className="h-64 overflow-y-scroll border p-2 mb-2">
         <div
           style={{ fontWeight: "bold", color: "green", textAlign: "center" }}
         >
           Messages
+          <div>
+            {receiverId && receiverId !== "ai-chat" ? (
+             <CallingFeature receiverId={receiverId} />
+            ) : (
+              <span style={{ color: "blue" }}>
+                {receiverId
+                  ? `Chat with ${receiverId}`
+                  : "Select a user to chat"}
+              </span>
+            )}
+          </div>
         </div>
         {receiverId === "ai-chat" ? (
           <div>
@@ -87,11 +106,11 @@ const ChatWindow = ({ receiverId }: { receiverId: string }) => {
               <div key={i} className="">
                 {"query" in msg && (
                   <>
-                    <div style={{ margin: "10px 4px", textAlign: "left" }}>
-                      <strong>AI:</strong> {(msg as TAIMessage).response}
-                    </div>
                     <div style={{ margin: "10px 4px", textAlign: "right" }}>
                       <strong>You:</strong> {(msg as TAIMessage).query}
+                    </div>
+                    <div style={{ margin: "10px 4px", textAlign: "left" }}>
+                      <strong>AI:</strong> {(msg as TAIMessage).response}
                     </div>
                   </>
                 )}
@@ -108,20 +127,28 @@ const ChatWindow = ({ receiverId }: { receiverId: string }) => {
       </div>
       <div className="flex gap-2">
         {receiverId && (
-          <div>
-            <input
-              value={input}
-              onChange={(e) => setInput(e.target.value)}
-              className="flex-1 border px-2 py-1 rounded"
-              placeholder="Type your message..."
-            />
-            <button
-              onClick={sendMessage}
-              className="bg-blue-500 text-white px-4 py-1 rounded"
-            >
-              Send
-            </button>
-          </div>
+          <>
+            {loading ? (
+              <div>
+                <span className="text-gray-500">Plz wait...</span>
+              </div>
+            ) : (
+              <div>
+                <input
+                  value={input}
+                  onChange={(e) => setInput(e.target.value)}
+                  className="flex-1 border px-2 py-1 rounded"
+                  placeholder="Type your message..."
+                />
+                <button
+                  onClick={sendMessage}
+                  className="bg-blue-500 text-white px-4 py-1 rounded"
+                >
+                  Send
+                </button>
+              </div>
+            )}
+          </>
         )}
       </div>
     </div>
@@ -129,3 +156,5 @@ const ChatWindow = ({ receiverId }: { receiverId: string }) => {
 };
 
 export default ChatWindow;
+
+
